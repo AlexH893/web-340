a607d9b23d33d230be2eaf0622d3751ec8ae7926
Alex <31377763+AlexH893@users.noreply.github.com>
2021-03-28 20:01:48 -0500

    adding index

---
+++ week-2/ejs-views/haefner-2.4.js
@@ -0,0 +1,39 @@
+/*
+============================================
+; Title: Assignment 2.4
+; Author: Alex Haefner
+; Date: 3-28-2021
+; Description: Sets up a webserver, using port 8080
+;===========================================
+*/
+var http = require("http");
+
+var express = require("express");
+
+var path = require("path");
+
+var app = express();
+
+//Tells Express the views are in the 'views' directory
+app.set("views", path.resolve(__dirname, "views"));
+
+//Tells Express to use the EJS view engine
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) 
+
+{
+
+   response.render("index", {
+    firstName: "Alex",
+    lastName: "Haefner",
+    address: "Iowa"
+   });
+
+});
+
+http.createServer(app).listen(8080, function() {
+
+    console.log("EJS-Views app started on port 8080.");
+
+});
